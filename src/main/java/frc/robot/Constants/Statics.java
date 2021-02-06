c61356a1c4aee01b09ad9a378fe4e19f2d068806
HeckDotExe <josephduffy00@gmail.com>
2021-02-06 12:16:55 -0500

    Added template

---
+++ src/main/java/frc/robot/Constants/Statics.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Constants;
+
+
+/** Add your docs here. */
+public class Statics {
+    public enum CompetitionSelection{
+        COMPETITION_1, COMPETITION_2, COMPETITION_3, COMPETITION_4, COMPETITION_5
+    }
+    //Change the "COMPETITION_1" in the next line to whichever competition you need to run.
+    static final public CompetitionSelection current_competition = CompetitionSelection.COMPETITION_1;
+
+
+}
